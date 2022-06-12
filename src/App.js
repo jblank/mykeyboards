6cd69e91ffb44d565b68c59a6beb1656eb16bdbe
Joseph M Blank <jblank@gmail.com>
2022-06-12 12:48:32 -0700

    Add fluent UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { DefaultButton, ThemeProvider, initializeIcons } from '@fluentui/react';
 
 function App() {
+  initializeIcons();
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <ThemeProvider>
+      <DefaultButton onClick={() => alert('hello')}>Hello World</DefaultButton>
+    </ThemeProvider>
+  );
 }
 
 export default App;
