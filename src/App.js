8c903589a6f7268020889cd7dc1b242c815de286
Joseph M Blank <jblank@gmail.com>
2022-06-12 12:52:23 -0700

    Remove extra var

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { DefaultButton, ThemeProvider, initializeIcons } from '@fluentui/react';
 
 function App() {
   initializeIcons();
-  const value = 'World';
   return (
     <ThemeProvider>
       <DefaultButton onClick={() => alert('hello')}>Hello World</DefaultButton>
